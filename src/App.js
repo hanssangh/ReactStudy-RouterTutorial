91cd580c54c7f7455355d97c013e4615a06546ef
hsh <hanssangh@naver.com>
2022-10-16 21:27:25 +0900

    중첩 라우트 사용

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Articles from './pages/Articles';
+import Article from './pages/Article';
+import Layout from './Layout';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/profile/:username' element={<Profile />} />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile/:username' element={<Profile />} />
+      </Route>
+      <Route path='/articles' element={<Articles />}>
+        <Route path=':id' element={<Article />} />
+      </Route>
     </Routes>
   );
 };
